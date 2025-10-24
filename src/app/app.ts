0907d66dee03b148a20110d08e08313c8f2fcf96
Sachin Sen <sachinsen1920@gmail.com>
2025-10-24 10:23:36 +0530

    Update app component to use external template and styles

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     // decorators
     selector: 'app-root',
-    template: `<p>Hello World</p>`,
-    standalone: true, // that we are noy using moduels
+    // template: `<p>Hello World</p>`,  // add only one template
+    standalone: true, // that we are not using moduels
+    templateUrl: './app.html',
+    styleUrls: ['./app.css'],
+    // styles: [''], // inline css
 })
 export class App {}
