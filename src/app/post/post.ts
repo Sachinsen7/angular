ab17d83625c63a49fdabc705fd4c3e0332aa59b9
Sachin Sen <sachinsen1920@gmail.com>
2025-10-25 13:01:41 +0530

    fix(post): add lifecycle hooks to app-post component

--- src/app/post/post.ts
+++ src/app/post/post.ts
@@ -1,4 +1,15 @@
-import { Component, input, output, OnInit } from '@angular/core';
+import {
+    Component,
+    input,
+    output,
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+} from '@angular/core';
 
 @Component({
     selector: 'app-post',
@@ -6,7 +17,16 @@ import { Component, input, output, OnInit } from '@angular/core';
     templateUrl: './post.html',
     styleUrl: './post.css',
 })
-export class Post implements OnInit {
+export class Post
+    implements
+        OnInit,
+        OnChanges,
+        DoCheck,
+        AfterContentChecked,
+        AfterContentInit,
+        AfterViewChecked,
+        AfterViewInit
+{
     postImage = input.required<string>(); // input property decorator
     imageSelected = output<string>(); // output property decorator
 
@@ -16,5 +36,31 @@ export class Post implements OnInit {
 
     ngOnInit() {
         console.log('ngonInit() called');
+    }
+
+    ngOnChanges() {
+        console.log('ngOnChanges() called');
+        // runs first
+    }
+
+    ngDoCheck() {
+        console.log('ngDoCheck() called');
+        // runs twice
+    }
+
+    ngAfterContentChecked(): void {
+        console.log('ngAfterContentChecked()called');
+    }
+
+    ngAfterContentInit(): void {
+        console.log('ngAfterContentInit()called');
+    }
+
+    ngAfterViewChecked(): void {
+        console.log('ngAfterViewChecked()called');
+    }
+
+    ngAfterViewInit(): void {
+        console.log('ngAfterViewInit()called');
     }
 }
